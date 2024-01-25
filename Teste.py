5081870e91c0e6a7a31bb15caae33f6796be8ec3
stennio.faria <stennio.faria@gmail.com>
2024-01-25 09:43:22 -0500

    commit 25/01/24

--- Teste.py
+++ Teste.py
@@ -1,6 +1,8 @@
+#from utilities import DRNAPTNcolors
 
-def gc_content(seq):
-    '''GC content in a DNA/RNA sequence'''
-    return ((seq.count('G') + seq.count('C'))/len(seq)*100)
+def DRNAPTNcolors(seq):
+    return str(seq).replace('A', '\033[1;31;43mA\033[m')\
 
-print(gc_content('GCAAAAAAAA'))
+t = DRNAPTNcolors('ATCG')
+
+print('\033[1;31;43mAAAAAAA\033[m')
